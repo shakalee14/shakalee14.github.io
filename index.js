3006525d6c73029f0e6a098018b2d57b60dca951
Devon Wesley <dwesley89@gmail.com>
2016-07-12 11:11:35 -0700

    added animation and controls to record img

--- index.js
+++ index.js
@@ -2,13 +2,21 @@ var myIndex = 0;
 carousel();
 
 function carousel() {
-    var i;
-    var x = document.getElementsByClassName("mySlides");
-    for (i = 0; i < x.length; i++) {
-       x[i].style.display = "none";
-    }
-    myIndex++;
-    if (myIndex > x.length) {myIndex = 1}
-    x[myIndex-1].style.display = "block";
-    setTimeout(carousel, 2000); // Change image every 2 seconds
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  myIndex++;
+  if (myIndex > x.length) {myIndex = 1}
+  x[myIndex-1].style.display = "block";
+  setTimeout(carousel, 2000); // Change image every 2 seconds
+}
+
+var record = document.querySelector('.record');
+record.onmousedown = function(){
+  record.style.animationPlayState = 'paused';
+}
+record.onmouseup = function(){
+  record.style.animationPlayState = 'running';
 }
