33706e6930539fd67dd65c984c2d799e631f8ed1
Shakalee14 <shakalee14@gmail.com>
2016-07-15 11:21:35 -0700

    Sun changes to highlighter

--- index.js
+++ index.js
@@ -8,33 +8,27 @@ function carousel() {
     x[i].style.display = "none";
   }
   myIndex++;
-  if (myIndex > x.length) {myIndex = 1}
-  x[myIndex-1].style.display = "block";
+  if (myIndex > x.length) {
+    myIndex = 1
+  }
+  x[myIndex - 1].style.display = "block";
   setTimeout(carousel, 2000); // Change image every 2 seconds
 }
-
 var modal = document.querySelector('.modal');
 //this is to open the modal for the headline to become a modal
-
 var headline = document.querySelector('.headline');
-
 headline.onclick = function() {
-  modal.style.display ='block';
-}
-
-//this is to close the modal for 
-
+    modal.style.display = 'block';
+  }
+  //this is to close the modal for 
 var closebtn = document.querySelector('.modal .closebtn');
-
 closebtn.onclick = function() {
   modal.style.display = 'none';
 };
-
-
 var record = document.querySelector('.record');
-record.onmousedown = function(){
+record.onmousedown = function() {
   record.style.animationPlayState = 'paused';
 }
-record.onmouseup = function(){
+record.onmouseup = function() {
   record.style.animationPlayState = 'running';
 }
